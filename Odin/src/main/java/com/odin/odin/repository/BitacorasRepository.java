9f8778b5355545847390ff7020ee44f9dad2df20
juancamilo343 <nievaocanojuancamilo@gmail.com>
2026-05-11 08:26:52 -0500

    Add files via upload

---
+++ Odin/src/main/java/com/odin/odin/repository/BitacorasRepository.java
@@ -0,0 +1,9 @@
+package com.odin.odin.repository;
+
+import com.odin.odin.model.Bitacoras;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BitacorasRepository extends JpaRepository<Bitacoras, Long>
+{
+
+}
