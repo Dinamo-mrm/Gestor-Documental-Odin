b7ca977b752814acd404aa4205d46b28affa4d1b
juancamilo343 <nievaocanojuancamilo@gmail.com>
2026-05-13 21:46:48 -0500

    Add files via upload

---
+++ Odin/src/main/java/com/odin/odin/repository/RadicadosRepository.java
@@ -0,0 +1,9 @@
+package com.odin.odin.repository;
+import com.odin.odin.model.Radicados;
+import org.jspecify.annotations.Nullable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RadicadosRepository extends JpaRepository<Radicados, Long>
+{
+
+}
