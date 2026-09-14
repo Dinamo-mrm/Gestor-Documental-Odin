4fc71c08930fc1dff24a33b73853fb021d70e174
Dinamo-mrm <carlozbarahona@hotmail.com>
2026-09-14 18:40:17 -0500

    feat(bitacora): agregar consultas para trazabilidad global y filtros

--- Odin/src/main/java/com/odin/odin/repository/HistorialRadicadoRepository.java
+++ Odin/src/main/java/com/odin/odin/repository/HistorialRadicadoRepository.java
@@ -5,5 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface HistorialRadicadoRepository extends JpaRepository<HistorialRadicado, Long> {
+
     List<HistorialRadicado> findById_radicadoOrderByFechaDesc(Long idRadicado);
+
+    List<HistorialRadicado> findById_usuarioOrderByFechaDesc(Long idUsuario);
+
+    List<HistorialRadicado> findByAccionIgnoreCaseOrderByFechaDesc(String accion);
+
+    List<HistorialRadicado> findTop100ByOrderByFechaDesc();
 }
