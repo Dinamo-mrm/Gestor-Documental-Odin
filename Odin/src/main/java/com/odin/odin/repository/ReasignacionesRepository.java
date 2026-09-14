f01858d95d662ff83a45add4cc56f567cd46e0f8
Dinamo-mrm <carlozbarahona@hotmail.com>
2026-09-13 22:39:32 -0500

    Completar consulta de reasignaciones por radicado

--- Odin/src/main/java/com/odin/odin/repository/ReasignacionesRepository.java
+++ Odin/src/main/java/com/odin/odin/repository/ReasignacionesRepository.java
@@ -2,7 +2,8 @@ package com.odin.odin.repository;
 
 import com.odin.odin.model.Reasignaciones;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface ReasignacionesRepository extends JpaRepository<Reasignaciones, Long> {
-
+    List<Reasignaciones> findById_radicadoOrderByFechaDesc(Integer idRadicado);
 }
