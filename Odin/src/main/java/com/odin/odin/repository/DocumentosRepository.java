d506706201d4f50c96f336b29665694f3f01a1fb
Dinamo-mrm <carlozbarahona@hotmail.com>
2026-09-13 21:41:31 -0500

    Agregar consulta de documentos por radicado

--- Odin/src/main/java/com/odin/odin/repository/DocumentosRepository.java
+++ Odin/src/main/java/com/odin/odin/repository/DocumentosRepository.java
@@ -1,10 +1,11 @@
 package com.odin.odin.repository;
 
 import com.odin.odin.model.Documentos;
-import org.jspecify.annotations.Nullable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DocumentosRepository extends JpaRepository<Documentos, Long>
-{
+import java.util.List;
 
+public interface DocumentosRepository extends JpaRepository<Documentos, Long> {
+
+    List<Documentos> findByIdRadicado(Long idRadicado);
 }
