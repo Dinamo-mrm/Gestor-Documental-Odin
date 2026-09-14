8afe37844009849d863edc712fdfcbe113c0a8a0
Dinamo-mrm <carlozbarahona@hotmail.com>
2026-09-13 21:41:35 -0500

    Corregir consulta JPQL de documentos por radicado

--- Odin/src/main/java/com/odin/odin/repository/DocumentosRepository.java
+++ Odin/src/main/java/com/odin/odin/repository/DocumentosRepository.java
@@ -2,10 +2,13 @@ package com.odin.odin.repository;
 
 import com.odin.odin.model.Documentos;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface DocumentosRepository extends JpaRepository<Documentos, Long> {
 
-    List<Documentos> findByIdRadicado(Long idRadicado);
+    @Query("SELECT d FROM Documentos d WHERE d.id_radicado = :radicado ORDER BY d.id_documento DESC")
+    List<Documentos> buscarPorRadicado(@Param("radicado") Long radicado);
 }
