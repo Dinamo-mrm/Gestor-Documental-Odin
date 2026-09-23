a5aa8b2c53442ce63203005e6ba50aff9aec9b33
Dinamo-mrm <carlozbarahona@hotmail.com>
2026-09-23 17:44:47 -0500

    refactor: simplificar repositorio de subseries CCD

--- Odin/src/main/java/com/odin/odin/repository/SubseriesRepository.java
+++ Odin/src/main/java/com/odin/odin/repository/SubseriesRepository.java
@@ -1,20 +1,11 @@
 package com.odin.odin.repository;
 
-import com.odin.odin.model.Series;
 import com.odin.odin.model.Subseries;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface SubseriesRepository extends JpaRepository<Subseries, Long> {
 
     Optional<Subseries> findByCodigoSubserie(String codigoSubserie);
-
-    @Query("select s.serie from Subseries s where s.codigo_subserie = :codigo")
-    Optional<Series> findSerieByCodigoSubserie(@Param("codigo") String codigo);
-
-    @Query("select s from Series s where s.codigo_serie = :codigo")
-    Optional<Series> findSerieByCodigo(@Param("codigo") String codigo);
 }
